d3fc1a51d336183ab4e639adf9a8b6bd4ac51361
Oakevinlee <klees19@gmail.com>
2022-11-03 22:02:02 -0700

    Heroku fix V2

--- routes/comments.js
+++ routes/comments.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var commentsCtrl = require('../controllers/comments');
-var loggedIn = require('../config/loggedIn');
+var ensureLoggedIn = require('../config/ensureLoggedIn');
 
 
-router.post('/courts/:id/comments', loggedIn, commentsCtrl.create);
-router.delete('/comments/:id', loggedIn, commentsCtrl.delete);
-router.get('/comments/:id/edit', loggedIn, commentsCtrl.edit);
-router.put('/courts/:courtId/comments/:id', loggedIn, commentsCtrl.update);
+router.post('/courts/:id/comments', ensureLoggedIn, commentsCtrl.create);
+router.delete('/comments/:id', ensureLoggedIn, commentsCtrl.delete);
+router.get('/comments/:id/edit', ensureLoggedIn, commentsCtrl.edit);
+router.put('/courts/:courtId/comments/:id', ensureLoggedIn, commentsCtrl.update);
 
 
 module.exports = router;
--- routes/courts.js
+++ routes/courts.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var courtsCtrl = require('../controllers/courts');
-var loggedIn = require('../config/loggedIn');
+var ensureLoggedIn = require('../config/ensureLoggedIn');
 
 
-router.post('/', loggedIn, courtsCtrl.create);
+router.post('/', ensureLoggedIn, courtsCtrl.create);
 router.get('/', courtsCtrl.index);
 router.get('/new', courtsCtrl.new);
 router.get('/:id', courtsCtrl.show);
-router.delete('/:id', loggedIn, courtsCtrl.delete);
+router.delete('/:id', ensureLoggedIn, courtsCtrl.delete);
 router.get('/:id/edit', courtsCtrl.edit);
 router.put('/:id', courtsCtrl.update);
 
